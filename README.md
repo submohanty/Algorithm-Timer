a551883d48b7c720a9037e02c06275c5c142ac2d
sub <sumo2640@colorado.edu>
2020-12-05 13:34:35 -0700

    Redid the repository because Git was messed up on my computer

---
+++ README.md
@@ -0,0 +1,12 @@
+# CSCI2275-Final-Project
+GitHub repository for storing my final project.
+
+Contributor: Sub Mohanty
+
+Initial Plan: Make something like a database with a menu controlled by a linked list with arrays within it, where you are a school administrator and want to store/get/write to a file/etc. data for your students. Each student will have their data stored in a linked list node, with arrays within each node to represent different data such as their SAT scores. Simple pseudocode:
+
+Student *s;
+
+cout << s->SATscores[3] << endl; // will print out their score on their 3rd attempt
+
+Other actions could be creating a text file with an individual student's data if a college requested it, adding a student's new data to the pre-existing text file, creating new students, creating a new tree of a student's past classes and their class trajectory, etc.
---
+++ Student.h
@@ -0,0 +1,53 @@
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// test
+// store each student in a BST based on their name
+// the BST sorts students by their name
+
+struct StandardizedTests{
+	vector<int> SATScores;
+	vector<int> ACTScores;
+};
+
+struct Clubs{
+};
+
+struct StudentNode{ 
+	// BST stuff
+	StudentNode *parent; 
+	StudentNode *left;
+	StudentNode *right;
+	// General information
+	string name;
+	int age;
+	int gradeInSchool;
+	// Academics
+	vector<StandardizedTests> testScores;
+
+	StudentNode(StudentNode *parent1, StudentNode *left1, StudentNode *right1, string name1, int age1, int gradeInSchool1){
+		parent = parent1;
+		left = left1;
+		right = right1;
+		name = name1;
+		age = age1;
+		gradeInSchool = gradeInSchool1;
+	}
+};
+
+class Student{
+	public:
+	private:
+};
+
+
+
+#endif 
